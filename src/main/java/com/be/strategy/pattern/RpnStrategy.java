20f0d9f5c67d914860598259cbe1a401b80639d7
ilovkatie <kacper.klepacz@gmail.com>
2017-12-11 22:32:30 +0100

    Resource injection replaced with constructor injection, post construct removed, map initialization more Java 8 style

--- src/main/java/com/be/strategy/pattern/RpnStrategy.java
+++ src/main/java/com/be/strategy/pattern/RpnStrategy.java
@@ -1,26 +1,25 @@
 package com.be.strategy.pattern;
 
 import com.be.strategy.pattern.operation.ArithmeticOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.Resource;
 import java.math.BigDecimal;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Component
 public class RpnStrategy {
 
-    @Resource
-    private List<ArithmeticOperation> arithmeticOperations;
     private Map<String, ArithmeticOperation> operations;
 
-    @PostConstruct
-    public void init() {
-        operations = new HashMap<>();
-        arithmeticOperations.forEach(o -> operations.put(o.getKey(), o));
+    @Autowired
+    RpnStrategy(List<ArithmeticOperation> arithmeticOperations) {
+        operations = arithmeticOperations
+                .stream()
+                .collect(Collectors.toMap(ArithmeticOperation::getKey, Function.identity()));
     }
 
     public String calculate(String firstToken, String secondToken, String operation) {
